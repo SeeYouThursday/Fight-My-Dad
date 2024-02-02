5aa1d03eb039620e67fad9a4f54e2fd2b93a9dd9
Kork <hail3yp@gmail.com>
2024-02-01 20:57:04 -0500

    adding return to landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,7 +1,11 @@
 const LandingPage = () => {
   // import into App to show on load '/'
   // Giant Fight My Dad img in the middle of page
-
+ return (
+    <h1>
+        what's up
+    </h1>
+ )
 };
 
 export default LandingPage;
