4238925246e9840ee70b12a48cca2b18d887bd2d
Madeline Urbanic <madelinerurbanic@gmail.com>
2024-02-01 19:27:39 -0500

    stats schema

--- server/models/Dad.js
+++ server/models/Dad.js
@@ -1,46 +1,46 @@
 const { Schema, model } = require('mongoose');
 
 const dadSchema = new Schema({
-  DadName: {
-    type: String,
-    required: true,
-  },
-  Nickname: {
-    type: String,
-    required: true,
-  },
-  Child: {
-    type: String,
-    required: true,
-  },
-  EntryMusic: {
-    type: String,
-    required: true,
-  },
-  DadJoke: {
-    type: String,
-    required: true,
-  },
-  Weight: {
-    type: Number,
-    default: 0,
-  },
-  ArmLength: {
-    type: Number,
-    default: 0,
-  },
-  Weapon: {
-    type: Number,
-    default: 0,
-  },
-  WinNum: {
-    type: Number,
-    default: 0,
-  },
-  LossNum: {
-    type: Number,
-    default: 0,
-  },
+    DadName: {
+        type: String,
+        required: true,
+    },
+    Nickname: {
+        type: String,
+        required: true,
+    },
+    Username: {
+        type: String,
+        required: true,
+    },
+    EntryMusic: {
+        type: String,
+        required: true,
+    },
+    DadJoke: {
+        type: String,
+        required: true,
+    },
+    Weight: {
+        type: Number,
+        default: 0,
+    },
+    ArmLength: {
+        type: Number,
+        default: 0,
+    },
+    Weapon: {
+        type: Number,
+        default: 0,
+    },
+    WinNum: {
+        type: Number,
+        default: 0,
+    },
+    LossNum: {
+        type: Number,
+        default: 0,
+    },
 });
 
 const Dad = model('Dad', dadSchema);
---
+++ server/models/Stats.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const statsSchema = new Schema({
+    Username: {
+        type: String,
+        required: true,
+    },
+    WinNum: {
+        type: Number,
+        default: 0,
+    },
+    LossNum: {
+        type: Number,
+        default: 0,
+    },
+});
+
+const Stats = model('Stats', statsSchema);
+
+module.exports = Stats;
