5da42fb4449d640ee59b35f5d585ea7f9c74f5be
Brian <galyenb@vcu.edu>
2024-02-02 14:34:39 -0500

    added FightMyDad graphic to the landing page TODO: signIn/signUp

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,15 @@
+// import Logo from '../assets/images/FIGHTMYDAD.svg';
+
+import '../assets/css/landing.css';
+
 const LandingPage = () => {
   // import into App to show on load '/'
   // Giant Fight My Dad img in the middle of page
   return (
-    <>
+    <div>
       <h1>My Dad is Better than your dad</h1>
-    </>
+      {/* <img src={Logo} /> */}
+    </div>
   );
 };
 
