762bb8ee9dc74de84a5d001309782389d05d6036
Brian <galyenb@vcu.edu>
2024-02-05 09:35:11 -0500

    added a stand-in navbar to be revised tbd/ added routing to the button on landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,14 +1,42 @@
-// import Logo from '../assets/images/FIGHTMYDAD.svg';
-
+// import Logo from '../assets/images/FIGHTMYDAD.gif';
+import Navigate from 'react';
 import '../assets/css/landing.css';
+import {
+  ButtonBase,
+  Box,
+  Container,
+  Grid,
+  // ImageButton,
+  // ImageBackdrop,
+  // ImageSrc,
+} from '@mui/material/';
+import Button from '@mui/material-next/Button';
+import Logo from '../assets/images/FIGHTMYDAD.gif';
+// import AltLogo from '../assets/images/output-onlinegiftools.gif';
+import StillLogo from '../assets/images/FMDBounce.png';
 
 const LandingPage = () => {
   // import into App to show on load '/'
   // Giant Fight My Dad img in the middle of page
   return (
     <div>
-      <h1>My Dad is Better than your dad</h1>
-      {/* <img src={Logo} /> */}
+      <Container fluid="true">
+        <Grid container={true}>
+          <Button
+            size="large"
+            variant="contained"
+            style={{
+              backgroundPosition: 'center',
+              display: 'flex',
+            }}
+            href="/dashboard"
+            // ?? Consider using a Link component from react-router-dom
+          >
+            <img src={Logo} alt="fightmydad logo" />
+          </Button>
+        </Grid>
+        <ButtonBase variant="contained">Bet!</ButtonBase>
+      </Container>
     </div>
   );
 };
