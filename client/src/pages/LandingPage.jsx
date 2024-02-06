69d7b31a58e703e0dff3bfe9207feefa318f8665
Brian <galyenb@vcu.edu>
2024-02-06 12:27:52 -0500

    fixed positioning of landing page btn

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,6 @@
 // import Logo from '../assets/images/FIGHTMYDAD.gif';
 import '../assets/css/landing.css';
-import {
-  ButtonBase,
-  Container,
-  Grid,
-  // ImageButton,
-  // ImageBackdrop,
-  // ImageSrc,
-} from '@mui/material/';
+import { ButtonBase, Container, Grid } from '@mui/material/';
 import Button from '@mui/material-next/Button';
 import Logo from '../assets/images/FIGHTMYDAD.gif';
 // import AltLogo from '../assets/images/output-onlinegiftools.gif';
@@ -18,17 +11,21 @@ const LandingPage = () => {
   // Giant Fight My Dad img in the middle of page
   return (
     <div>
-      <Container fluid="true">
-        <Grid container={true}>
+      <Container>
+        <Grid>
           <Button
             size="large"
             variant="contained"
             style={{
               backgroundPosition: 'center',
               display: 'flex',
+              overflow: 'hidden',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
             href="/dashboard"
             // ?? Consider using a Link component from react-router-dom
+            //! Change Background color of img
           >
             <img src={Logo} alt="fightmydad logo" />
           </Button>
