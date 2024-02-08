da7a1cca2077fc49c978642c78f12e93c9d8c348
Brian <galyenb@vcu.edu>
2024-02-07 20:18:26 -0500

    added typeDefs, resolvers TODO: set up front end queries/mutations, test out QsMs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,8 +3,14 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    user: async () => {
-      return User.find({});
+    me: async (parent, { userId, username }) => {
+      try {
+        return User.findOne({
+          $or: [{ _id: userId }, { username: username }],
+        });
+      } catch (err) {
+        console.log(err);
+      }
     },
     dad: async (parent, { _id }) => {
       const params = _id ? { _id } : {};
@@ -26,39 +32,42 @@ const resolvers = {
         throw AuthenticationError;
       }
     },
-  },
-  loginUser: async (parent, { username, password }) => {
-    const user = await User.findOne({ username });
+    loginUser: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
 
-    if (!user) {
-      throw new AuthenticationError('User not found. Do you have an account?');
-    }
-
-    const correctPw = await user.isCorrectPassword(password);
-
-    if (!correctPw) {
-      throw new AuthenticationError('Incorrect credentials!');
-    }
-
-    const token = signToken(user);
-    console.log('Logged IN');
-    return { token, user };
-  },
-  saveDads: async (parent, { dad }, context) => {
-    if (context.user) {
-      try {
-        return User.findOneAndUpdate(
-          { _id: context.user._id },
-          {
-            $addToSet: { savedDads: { dad } },
-          },
-          { new: true, runValidators: true }
+      if (!user) {
+        throw new AuthenticationError(
+          'User not found. Do you have an account?'
         );
-      } catch (err) {
-        // If user attempts to execute this mutation and isn't logged in, throw an error
-        throw new AuthenticationError('User is not authenticated.');
       }
-    }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials!');
+      }
+
+      const token = signToken(user);
+      console.log('Logged IN');
+      return { token, user };
+    },
+    saveDad: async (parent, { dad }, context) => {
+      if (context.user) {
+        try {
+          return User.findOneAndUpdate(
+            { _id: context.user._id },
+            {
+              $addToSet: { savedDads: { dad } },
+            },
+            { new: true, runValidators: true }
+          );
+        } catch (err) {
+          // If user attempts to execute this mutation and isn't logged in, throw an error
+          throw new AuthenticationError('User is not authenticated.');
+        }
+      }
+    },
+    removeDad: async (parent, { dad }) => {},
   },
 };
 
