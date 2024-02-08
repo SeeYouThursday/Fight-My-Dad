35c9bb9b91017576f27274c5faf50a981d54ddca
Madeline Urbanic <madelinerurbanic@gmail.com>
2024-02-07 19:39:50 -0500

    basics

--- client/src/pages/DadCreate.jsx
+++ client/src/pages/DadCreate.jsx
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
 import { useMutation } from 'react';
-import Btn from '../Components/Btn'
+import Btn from '../Components/Btn';
+import { Grid, TextField, Button } from '@mui/material/';
+
 
 const DadCreate = () => {
+  const [dadName, setDadName] = useState('');
+
+  const handleDadChange = (event) => {
+    setDadName(event.target.value);
+  };
+
+  const [nickname, setNickname] = useState('');
+
+  const handleNicknameChange = (event) => {
+    setNickname(event.target.value);
+  };
   //card component
   //closebtn componet - top right
   //btn component - submit
@@ -10,9 +24,73 @@ const DadCreate = () => {
     const [createDad] = useMutation(CREATE_DAD);
   };
 
+  
+
   return (
   <>
-   <Btn />
+  <Btn />
+  <Grid>
+    <input
+        type="text"
+        value={dadName}
+        onChange={handleDadChange}
+        placeholder="Enter something..."
+      />
+      <input
+        type="text"
+        value={nickname}
+        onChange={handleNicknameChange}
+        placeholder="Enter something..."
+      />
+      <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+      <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+      <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+        <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+      <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+        <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+        <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+        <input
+        type="text"
+        value={dadName}
+        onChange={handleInputChange}
+        placeholder="Enter something..."
+      />
+  </Grid>
   </>
   );
 };
