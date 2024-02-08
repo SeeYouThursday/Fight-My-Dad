8f59951e1345faebdae24c87c642e1fb8c8add48
Brian <galyenb@vcu.edu>
2024-02-07 19:11:01 -0500

    added base server code/addUser&loginUser mutations are complete TODO: set up typeDefs to begin running tests on AS

--- server/models/Dad.js
+++ server/models/Dad.js
@@ -1,52 +1,52 @@
 const { Schema, model } = require('mongoose');
 
 const dadSchema = new Schema({
-    DadName: {
-        type: String,
-        required: true,
-    },
-    Nickname: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    Username: {
-        type: Number,
-        required: true,
-    },
-    EntryMusic: {
-        type: String,
-        required: true,
-    },
-    DadJoke: {
-        type: String,
-        required: true,
-    },
-    Weight: {
-        type: Number,
-        default: 0,
-    },
-    ArmLength: {
-        type: Number,
-        default: 0,
-    },
-    Weapon: {
-        type: Number,
-        default: 0,
-    },
-    WinNum: {
-        type: Number,
-        default: 0,
-    },
-    LossNum: {
-        type: Number,
-        default: 0,
+  DadName: {
+    type: String,
+    required: true,
+  },
+  Nickname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Username: {
+    type: Number,
+    required: true,
+  },
+  EntryMusic: {
+    type: String,
+    required: true,
+  },
+  DadJoke: {
+    type: String,
+    required: true,
+  },
+  Weight: {
+    type: Number,
+    default: 0,
+  },
+  ArmLength: {
+    type: Number,
+    default: 0,
+  },
+  Weapon: {
+    type: Number,
+    default: 0,
+  },
+  WinNum: {
+    type: Number,
+    default: 0,
+  },
+  LossNum: {
+    type: Number,
+    default: 0,
     Icon: {
-        type: String,
-    }
+      type: String,
     },
+  },
 });
 
-const Dad = model('Dad', dadSchema);
+// const Dad = model('Dad', dadSchema); //! Changed to just a schema instead of a model to use in reference
 
-module.exports = Dad;
+module.exports = dadSchema;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Stats, Dad } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -9,17 +10,55 @@ const resolvers = {
       const params = _id ? { _id } : {};
       return Dad.find(params);
     },
-
-    }
   },
   Mutation: {
-    // login_user
-    login_user: async (parent, args) => {
-      const user = await User.create(args);
-      return user;
+    addUser: async (parent, { firstName, lastName, username, password }) => {
+      try {
+        const user = await User.create({
+          firstName,
+          lastName,
+          username,
+          password,
+        });
+        const token = signToken(user);
+        return { token, user };
+      } catch (err) {
+        throw AuthenticationError;
+      }
     },
+  },
+  loginUser: async (parent, { username, password }) => {
+    const user = await User.findOne({ username });
 
-    },
+    if (!user) {
+      throw new AuthenticationError('User not found. Do you have an account?');
+    }
+
+    const correctPw = await user.isCorrectPassword(password);
+
+    if (!correctPw) {
+      throw new AuthenticationError('Incorrect credentials!');
+    }
+
+    const token = signToken(user);
+    console.log('Logged IN');
+    return { token, user };
+  },
+  saveDads: async (parent, { dad }, context) => {
+    if (context.user) {
+      try {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: { savedDads: { dad } },
+          },
+          { new: true, runValidators: true }
+        );
+      } catch (err) {
+        // If user attempts to execute this mutation and isn't logged in, throw an error
+        throw new AuthenticationError('User is not authenticated.');
+      }
+    }
   },
 };
 
