92c224fb2fe150c8647ce577e3ba74ed81e79f5e
Kork <hail3yp@gmail.com>
2024-02-10 00:11:48 -0500

    added Auth.loggedIn to dashboard (NO LONGER BUGGED! not functional though :(

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import Nav from '../Components/Nav';
 import Image from '../Components/Image';
 import DadCard from '../Components/Card';
-import Auth from "../utils/auth.js"
+import Auth from '../utils/auth.js'
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries'
 
@@ -42,35 +42,28 @@ const Dashboard = (me) => {
   const { loading, data } = useQuery(QUERY_ME)
 
   return (
-    {Auth.loggedIn() ? (
-    <main>
-      <div style={styles.container}>
-        <section style={styles.mainSection}>
-          <Image />
-          <div>
-            <h1 style={styles.user}>{me.username}</h1>
-            <h3>Hey, {me.firstName} {me.lastName}!</h3>
+    <>
+      {Auth.loggedIn() ? (
+        <main>
+          <div style={styles.container}>
+            <section style={styles.mainSection}>
+              <Image />
+              <div>
+                <h1 style={styles.user}>{me.username}</h1>
+                <h3>Hey, {me.firstName} {me.lastName}!</h3>
+              </div>
+            </section>
           </div>
-          
-        </section>
-      </div>
-
-      <div style={styles.divider}></div>
-
-      {/* PUT DAD COLLECTION HERE */}
-
-
-      <DadCard />
-        {/* {dads.map((dad) => (
-          <Stat />
-        ))} */}
-
-
-    </main>
-    ) : (
-      <p>hey! Log in loser!</p>
-    )
+          <div style={styles.divider}></div>
+          <DadCard />
+        </main>
+       ) : (
+      <p>hi</p>
+      )}
+    </>
+    
   );
+
 };
 
 export default Dashboard;
