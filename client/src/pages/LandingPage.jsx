098ebcde42c87cf5ea5181ff6c3a6f8e57de12c0
Kork <hail3yp@gmail.com>
2024-02-10 00:26:27 -0500

    added login! button and sign up! button onto landing page and replacing AI image

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -7,6 +7,20 @@ import Logo from '../assets/images/FIGHTMYDAD.gif';
 // import StillLogo from '../assets/images/FMDBounce.png';
 import FMDLanding from '../assets/images/FMDLanding.png';
 
+const styles = {
+  btn: {
+    fontSize: '50px',
+    fontFamily: 'Permanent Marker',
+    backgroundColor: '#fedb0e',
+    padding: '20px',
+    borderRadius: '10px',
+    minWidth: '300px'
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  }
+}
 const LandingPage = () => {
   // import into App to show on load '/'
   // Giant Fight My Dad img in the middle of page
@@ -24,29 +38,31 @@ const LandingPage = () => {
         alignItems: 'center',
       }}
     >
-      <Container>
-        <Grid>
-          <Button
-            // size="large"
-            variant="contained"
-            // style={{
-            //   backgroundPosition: 'center',
-            //   display: 'flex',
-            //   overflow: 'hidden',
-            //   justifyContent: 'center',
-            //   alignItems: 'center',
-            // }}
-            href="/dashboard"
-            // ?? Consider using a Link component from react-router-dom
-            //! Change Background color of img
-          >
-            {/* <img src={Logo} alt="fightmydad logo" /> */}
-          </Button>
-        </Grid>
-        <ButtonBase variant="contained" href="/login">Bet!</ButtonBase>
+      <Container style={styles.container}>
+        <ButtonBase variant="contained" style={styles.btn} href="/login">Login!</ButtonBase>
+        <ButtonBase variant="contained" style={styles.btn} href="/signup">Sign Up!</ButtonBase>
       </Container>
     </div>
   );
 };
 
 export default LandingPage;
+
+{/* <Grid>
+<Button */}
+  // size="large"
+  // variant="contained"
+  // style={{
+  //   backgroundPosition: 'center',
+  //   display: 'flex',
+  //   overflow: 'hidden',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // }}
+  // href="/dashboard"
+  // ?? Consider using a Link component from react-router-dom
+  //! Change Background color of img
+// >
+  {/* <img src={Logo} alt="fightmydad logo" /> */}
+// </Button>
+// </Grid>
