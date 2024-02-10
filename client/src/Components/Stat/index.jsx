c54cf72349bed2b0c12c1cd8f790c400bbf689a2
Tiffani Underwood <tdunderwood217@gmail.com>
2024-02-09 23:35:12 -0500

    added avatar components to stat page needs debugged to display individual pictures or initials

--- client/src/Components/Stat/index.jsx
+++ client/src/Components/Stat/index.jsx
@@ -8,6 +8,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -38,11 +39,11 @@ function createData(name, joke, win, loss) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('lk', 159, 6.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Gingerbread', 356, 16.0, 49),
 ];
 
 export default function Stat() {
@@ -51,23 +52,39 @@ export default function Stat() {
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
+            <StyledTableCell>Avatar
+            </StyledTableCell>
             <StyledTableCell>Name</StyledTableCell>
-            <StyledTableCell align="right">Dad Joke</StyledTableCell>
-            {/* <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell> */}
+            <StyledTableCell>Dad Joke</StyledTableCell>
             <StyledTableCell align="right">Wins</StyledTableCell>
             <StyledTableCell align="right">Losses</StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <StyledTableRow key={row.name}>
+              <StyledTableCell>
+                <StyledTableRow>
+                  
+                </StyledTableRow>
+                <Avatar 
+                alt="John Doe"
+                src="../client/src/assets/images/pexels-photo-91227.jpeg" 
+                sx={{bgcolor: cyan[500]}}>DL</Avatar>
+                {/* <Avatar
+                alt="Rick Caruso"
+                src="../client/src/assets/images/pexels-photo-91227.jpeg"
+                sx={{bgcolor: cyan[100]}}>RC</Avatar> */}
+              </StyledTableCell>
               <StyledTableCell component="th" scope="row">
                 {row.name}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.joke}</StyledTableCell>
+              <StyledTableCell>{row.joke}</StyledTableCell>
               {/* <StyledTableCell align="right">{row.fat}</StyledTableCell> */}
               <StyledTableCell align="right">{row.win}</StyledTableCell>
               <StyledTableCell align="right">{row.loss}</StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
@@ -75,12 +92,4 @@ export default function Stat() {
     </TableContainer>
   );
 }
-
-// export default function Stat() {
-//     return(
-//         <div className='stat'>
-//             {/* TO DO: CREATE STAT HERE */}
-//         </div>
-//     )
-// }
 
