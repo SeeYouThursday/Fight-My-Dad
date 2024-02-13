579fffa86940d4a86afc053fa49d13c3a4e3a86b
Madeline Urbanic <madelinerurbanic@gmail.com>
2024-02-12 21:34:03 -0500

    reformat

--- client/src/pages/FightBefore.jsx
+++ client/src/pages/FightBefore.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Btn from "../Components/Btn";
 import { useQuery, useMutation } from "@apollo/client";
 import {
